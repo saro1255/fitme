89fe98874018aabadde7f06fae6b9dcfcdf4bd68
sarou1255 <sarou1255@gmail.com>
2025-07-02 13:26:10 +0100

    checkout final

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 let jsonData = {};
+let stored_data = {};
 
 (async function () {
     const front_whole_container = document.querySelector(".right-content");
@@ -178,7 +179,7 @@ function search_dish(categ) {
 }
 
 function div_functionality(div_id){
-    var div_id_array = ['main_div','checkout_div','pay_div']
+    var div_id_array = ['main_div','checkout_div','show_cart']
     for(var i in div_id_array){
         if(div_id_array[i] != div_id){
             $(`#${div_id_array[i]}`).removeClass('show_div')
@@ -290,7 +291,14 @@ function selected_meals(meal_categ, meal){
                                         </div>
                                         <span class="cart_desc">Extra charges may apply</span>
                                     </div>
-                                    <button class="cart_btn">Checkout</button>
+                                    <button class="cart_btn" onclick='add_to_backet("${meal_categ}", "${meal}")'>
+                                        <svg width="30px" viewBox="-2.4 -2.4 28.80 28.80" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="" transform="matrix(1, 0, 0, 1, 0, 0)rotate(0)">
+                                            <g id="SVGRepo_bgCarrier" stroke-width="0"/>
+                                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#ffffffCCCCCC" stroke-width="0.096"/>
+                                            <g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M14 2C14 1.44772 13.5523 1 13 1C12.4477 1 12 1.44772 12 2V8.58579L9.70711 6.29289C9.31658 5.90237 8.68342 5.90237 8.29289 6.29289C7.90237 6.68342 7.90237 7.31658 8.29289 7.70711L12.2929 11.7071C12.6834 12.0976 13.3166 12.0976 13.7071 11.7071L17.7071 7.70711C18.0976 7.31658 18.0976 6.68342 17.7071 6.29289C17.3166 5.90237 16.6834 5.90237 16.2929 6.29289L14 8.58579V2ZM1 3C1 2.44772 1.44772 2 2 2H2.47241C3.82526 2 5.01074 2.90547 5.3667 4.21065L5.78295 5.73688L7.7638 13H18.236L20.2152 5.73709C20.3604 5.20423 20.9101 4.88998 21.4429 5.03518C21.9758 5.18038 22.29 5.73006 22.1448 6.26291L20.1657 13.5258C19.9285 14.3962 19.1381 15 18.236 15H8V16C8 16.5523 8.44772 17 9 17H16.5H18C18.5523 17 19 17.4477 19 18C19 18.212 18.934 18.4086 18.8215 18.5704C18.9366 18.8578 19 19.1715 19 19.5C19 20.8807 17.8807 22 16.5 22C15.1193 22 14 20.8807 14 19.5C14 19.3288 14.0172 19.1616 14.05 19H10.95C10.9828 19.1616 11 19.3288 11 19.5C11 20.8807 9.88071 22 8.5 22C7.11929 22 6 20.8807 6 19.5C6 18.863 6.23824 18.2816 6.63048 17.8402C6.23533 17.3321 6 16.6935 6 16V14.1339L3.85342 6.26312L3.43717 4.73688C3.31852 4.30182 2.92336 4 2.47241 4H2C1.44772 4 1 3.55228 1 3ZM16 19.5C16 19.2239 16.2239 19 16.5 19C16.7761 19 17 19.2239 17 19.5C17 19.7761 16.7761 20 16.5 20C16.2239 20 16 19.7761 16 19.5ZM8 19.5C8 19.2239 8.22386 19 8.5 19C8.77614 19 9 19.2239 9 19.5C9 19.7761 8.77614 20 8.5 20C8.22386 20 8 19.7761 8 19.5Z" fill="#ffffff"/> </g>
+                                        </svg>
+                                        Add to Basket
+                                    </button>
                                 </div>
                             </div>
                         </div>`
@@ -309,8 +317,8 @@ function ctrl_qty(mode, meal_categ, meal){
         current_qty = current_qty
     }
 
-    if(current_qty < 1){
-        current_qty = 1
+    if(current_qty < 0){
+        current_qty = 0
     }
 
     $("#show_checkout_qty").text(current_qty)
@@ -324,4 +332,36 @@ function ctrl_qty(mode, meal_categ, meal){
         show_subtot = ((parseInt(current_qty)/2) * parseFloat(get_offer))
     }   
     $('#show_checkout_subtot').text(show_subtot)
+}
+
+function add_to_backet(meal_categ, meal){
+    var current_qty = $("#show_checkout_qty").text()
+    var current_amount = $('#show_checkout_subtot').text()
+    if(!stored_data[meal_categ])
+        stored_data[meal_categ] = {}
+    if(!stored_data[meal_categ][meal])
+        stored_data[meal_categ][meal] = {}
+
+    stored_data[meal_categ][meal] = {
+        qty : current_qty,
+        amt : current_amount
+    }
+
+    show_notify('success', meal, `Food successfully added to the Basket`);
+}
+
+function show_notify(alert,title,text){
+    Swal.fire({
+        icon: alert,
+        title: title,
+        text: text,
+        timer: 2000,
+        timerProgressBar: true,
+        showConfirmButton: false
+    });
+}
+
+function show_cart(){
+    div_functionality('show_cart')
+    var show_cart_div = ``
 }
